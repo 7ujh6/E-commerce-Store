68eeb620f31b33f145ea811b39bdcb3e759be922
stant <stantondobson@gmail.com>
2020-04-10 14:30:38 -0400

    removed last console log

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -6,7 +6,6 @@ import './collection.styles.scss';
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
 const CollectionPage = ({collection}) => {
-    console.log ("collection page rendering! collection object:  ", collection);
     const {title, items} = collection;
     return <div className='collection-page'><h2 className='title'>{title}</h2>
     <div className='items'>{items.map(item => (<CollectionItem key={item.id} item={item}/>))}</div></div>
